fb1642e368a5fdf1635faf6f5c563fdc74e1fc12
Miguel Maidana <maidana.miguell@gmail.com>
2020-05-14 19:30:45 -0300

    comienzo con el componente Ordenes , traigo las ordenes de firebase

--- src/components/paginas/Ordenes.js
+++ src/components/paginas/Ordenes.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React,{useState,useEffect,useContext} from 'react';
+import {FirebaseContext} from "../../firebase/index"
+
 
 const Ordenes = props => {
+
+    // context con las operacion de Firebase
+
+    const {firebase} = useContext(FirebaseContext)
+
+    // state con las ordenes
+
+    const [ordenes,guardarOrdenes] = useState([])
+
+    useEffect(()=>{
+
+        const obtenerOrdene =()=>{
+            firebase.db.collection("ordenes").where("completado","==",false).onSnapshot(manejarSnapshot);
+        }
+
+        obtenerOrdene()
+
+    },[firebase.db])
+
+    function manejarSnapshot (snapshot) {
+        const ordenes = snapshot.docs.map(doc =>{
+            return {
+                id : doc.id,
+                ...doc.data()
+            }
+        })
+        guardarOrdenes(ordenes)
+    }
+
     return (
         <>
             <h1 className="text-3xl font-light mb-4">Ordenes</h1>
